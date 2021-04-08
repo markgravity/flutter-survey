c85074add865ae7154b2cbbc529e1f45e0e95090
MarkG <markgravity.in@gmail.com>
2021-04-08 13:11:11 +0700

    chore: add flavors (#2)

--- lib/main_staging.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:survey/app.dart';
-import 'package:survey/core/environment.dart';
-
-void main() {
-  Environment.staging.set();
-  runApp(App());
-}
