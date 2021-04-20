e93939773481c7e5b283002168cc1065c90ec216
MarkG <markgravity.in@gmail.com>
2021-04-20 17:11:37 +0700

    feat: add hud progress (#21)
    - fix incorrect ui in android

--- lib/components/progress_hud/progress_hud.dart
+++ lib/components/progress_hud/progress_hud.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
@@ -13,18 +14,29 @@ class ProgressHUD extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget? progressIndicator;
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        progressIndicator = PlatformCircularProgressIndicator();
+        break;
+      default:
+        progressIndicator = Container(
+          width: 100,
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          child: PlatformCircularProgressIndicator(
+            cupertino: (_, __) => CupertinoProgressIndicatorData(radius: 20),
+          ),
+        );
+    }
+
     return ModalProgressHUD(
       inAsyncCall: isShow,
-      progressIndicator: Container(
-        width: 100,
-        height: 100,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(12))),
-        child: PlatformCircularProgressIndicator(
-          cupertino: (_, __) => CupertinoProgressIndicatorData(radius: 20),
-        ),
-      ),
+      progressIndicator: progressIndicator,
       child: child,
     );
   }
