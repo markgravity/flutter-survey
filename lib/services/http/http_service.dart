5a70615a15717c091651bd78cb1adcc3509f0280
MarkG <markgravity.in@gmail.com>
2021-04-20 14:24:08 +0700

    chore: add http service (#10)

---
+++ lib/services/http/http_service.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:enumerated_class/enumerated_class.dart';
+
+part 'http_method.dart';
+part 'http_exception.dart';
+part 'http_response.dart';
+
+abstract class HttpService {
+  Future<dynamic> request({
+    required HttpMethod method,
+    dynamic? data,
+    required String url,
+    Map<String, dynamic>? headers,
+  });
+}
+
+class HttpServiceImpl implements HttpService {
+  final _dio = Dio();
+
+  HttpServiceImpl() {
+    _dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+  }
+
+  @override
+  Future<dynamic> request({
+    required HttpMethod method,
+    dynamic? data,
+    required String url,
+    Map<String, dynamic>? headers,
+  }) async {
+    final options = Options(method: method.rawValue, headers: headers);
+    try {
+      final response = await _dio.request(
+        url,
+        data: data,
+        options: options,
+      );
+      return response.data;
+    } on DioError catch (e) {
+      throw HttpException.fromDioError(e);
+    }
+  }
+}
