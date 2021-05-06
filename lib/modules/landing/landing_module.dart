c645a1abcd685663f7a2495ea132f129fcf9f2a4
MarkG <markgravity.in@gmail.com>
2021-05-06 10:23:53 +0700

    feat: integrate landing screen (#14)

--- lib/modules/landing/landing_module.dart
+++ lib/modules/landing/landing_module.dart
@@ -4,7 +4,12 @@ import 'package:streams_provider/streams_provider.dart';
 import 'package:survey/components/alert/alert.dart';
 import 'package:survey/core/viper/module.dart';
 import 'package:survey/gen/assets.gen.dart';
+import 'package:survey/modules/home/home_module.dart';
+import 'package:survey/modules/login/login_module.dart';
 import 'package:survey/modules/screen.dart';
+import 'package:survey/core/extensions/build_context.dart';
+import 'package:survey/repositories/auth_repository.dart';
+import 'package:survey/services/locator/locator_service.dart';
 
 part 'landing_presenter.dart';
 
--- lib/modules/landing/landing_presenter.dart
+++ lib/modules/landing/landing_presenter.dart
@@ -5,6 +5,19 @@ abstract class LandingPresenter
 
 class LandingPresenterImpl extends LandingPresenter
     implements LandingInteractorDelegate, LandingViewDelegate {
+  LandingPresenterImpl() {
+    stateDidInit.voidListen(_stateDidInit).addTo(disposeBag);
+    alertDialogDidClose.voidListen(_alertDialogDidClose).addTo(disposeBag);
+    authenticationDidFailToValidate
+        .listen(_authenticationDidFailToValidate)
+        .addTo(disposeBag);
+
+    animationDidFinish
+        .zipWith<bool, bool>(authenticationDidValidate, (t, s) => s)
+        .listen(_didAllFinish)
+        .addTo(disposeBag);
+  }
+
   @override
   final stateDidInit = BehaviorSubject<void>();
 
@@ -13,4 +26,34 @@ class LandingPresenterImpl extends LandingPresenter
 
   @override
   final animationDidFinish = BehaviorSubject<void>();
+
+  @override
+  final authenticationDidFailToValidate = BehaviorSubject<Object>();
+
+  @override
+  final authenticationDidValidate = BehaviorSubject<bool>();
+
+  void _stateDidInit() {
+    view.beginAnimation();
+    interactor.validateAuthentication();
+  }
+
+  void _alertDialogDidClose() {
+    interactor.validateAuthentication();
+  }
+
+  void _authenticationDidFailToValidate(Object error) {
+    view.alert(error);
+  }
+
+  void _didAllFinish(bool isAuthenticated) {
+    switch (isAuthenticated) {
+      case true:
+        router.replaceToHomeScreen(context: view.context);
+        break;
+      case false:
+        router.replaceToLoginScreen(context: view.context);
+        break;
+    }
+  }
 }
