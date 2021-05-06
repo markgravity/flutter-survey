236652f2d82e6c01a1d0839775a60b2b00d6c507
MarkG <markgravity.in@gmail.com>
2021-05-06 10:43:19 +0700

    feat: create ui for login screen (#9)

--- lib/services/locator/locator_service_register.dart
+++ lib/services/locator/locator_service_register.dart
@@ -17,5 +17,10 @@ class LocatorServiceRegister {
     locator.registerFactory<LandingInteractor>(() => LandingInteractorImpl());
     locator.registerFactory<LandingRouter>(() => LandingRouterImpl());
     locator.registerFactory<LandingPresenter>(() => LandingPresenterImpl());
+
+    // Login
+    locator.registerFactory<LoginInteractor>(() => LoginInteractorImpl());
+    locator.registerFactory<LoginRouter>(() => LoginRouterImpl());
+    locator.registerFactory<LoginPresenter>(() => LoginPresenterImpl());
   }
 }
