baec471d9dc77a12084c1613050fc73bb93adb77
MarkG <markgravity.in@gmail.com>
2021-05-06 10:25:23 +0700

    chore: write tests for landing screen (#14)

---
+++ test/modules/landing/landing_interactor_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:quick_test/quick_test.dart';
+import 'package:survey/modules/landing/landing_module.dart';
+import 'package:survey/repositories/auth_repository.dart';
+import 'package:survey/services/locator/locator_service.dart';
+
+import '../../helpers/behavior_subject_generator.dart';
+import 'landing_interactor_test.mocks.dart';
+
+@GenerateMocks([AuthRepository, LandingInteractorDelegate])
+void main() {
+  describe("an Landing interactor", () {
+    late LandingInteractor interactor;
+    late MockAuthRepository authRepository;
+    late MockLandingInteractorDelegate delegate;
+    late BehaviorSubjectGenerator generator;
+
+    beforeEach(() {
+      generator = BehaviorSubjectGenerator();
+
+      delegate = MockLandingInteractorDelegate();
+      when(delegate.authenticationDidValidate)
+          .thenAnswer((realInvocation) => generator.make(0));
+      when(delegate.authenticationDidFailToValidate)
+          .thenAnswer((realInvocation) => generator.make(1));
+
+      authRepository = MockAuthRepository();
+      locator.registerFactory<AuthRepository>(() => authRepository);
+      interactor = LandingInteractorImpl();
+      interactor.delegate = delegate;
+    });
+
+    describe("it's validateAuthentication() is called", () {
+      beforeEach(() {
+        when(authRepository.isAuthenticated).thenReturn(true);
+      });
+
+      context("when authRepository returns success", () {
+        beforeEach(() {
+          when(authRepository.attemptAndFetchUser())
+              .thenAnswer((realInvocation) => Future.value(null));
+          interactor.validateAuthentication();
+        });
+
+        it("triggers delegate's didValidateAuthentication emits true", () {
+          expect(delegate.authenticationDidValidate, emits(true));
+        });
+      });
+
+      context("when authRepository returns failure", () {
+        final error = Exception();
+        beforeEach(() {
+          when(authRepository.attemptAndFetchUser())
+              .thenAnswer((_) => Future.error(error));
+          interactor.validateAuthentication();
+        });
+
+        it("triggers delegate's didValidateAuthentication emits error", () {
+          expect(delegate.authenticationDidFailToValidate, emits(error));
+        });
+      });
+    });
+  });
+}
---
+++ test/modules/landing/landing_presenter_test.dart
@@ -0,0 +1,96 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:quick_test/quick_test.dart';
+import 'package:survey/modules/landing/landing_module.dart';
+
+import '../../mocks/build_context.dart';
+import 'landing_presenter_test.mocks.dart';
+
+@GenerateMocks([LandingView, LandingRouter, LandingInteractor])
+void main() {
+  describe("a Landing presenter", () {
+    late LandingPresenterImpl presenter;
+    late MockLandingView view;
+    late MockLandingRouter router;
+    late MockLandingInteractor interactor;
+    late MockBuildContext buildContext;
+
+    beforeEach(() {
+      buildContext = MockBuildContext();
+
+      view = MockLandingView();
+      when(view.context).thenReturn(buildContext);
+
+      router = MockLandingRouter();
+
+      interactor = MockLandingInteractor();
+
+      presenter = LandingPresenterImpl();
+      presenter.configure(view: view, interactor: interactor, router: router);
+    });
+
+    describe("it receives didInitState event", () {
+      beforeEach(() {
+        presenter.stateDidInit.add(null);
+      });
+
+      it("triggers view to begin animation", () {
+        verify(view.beginAnimation()).called(1);
+      });
+
+      it("triggers interactor to validate authentication", () {
+        verify(interactor.validateAuthentication()).called(1);
+      });
+    });
+
+    describe("it receives didCloseAlertDialog event", () {
+      beforeEach(() {
+        presenter.alertDialogDidClose.add(null);
+      });
+
+      it("triggers interactor to validate authentication", () {
+        verify(interactor.validateAuthentication()).called(1);
+      });
+    });
+
+    describe("it receives didFailToValidateAuthentication event", () {
+      beforeEach(() {
+        presenter.authenticationDidFailToValidate.add(Exception());
+      });
+
+      it("triggers view to show alert dialog", () {
+        verify(view.alert(any)).called(1);
+      });
+    });
+
+    context(
+        "it receives didFinishAnimation and didValidateAuthentication events",
+        () {
+      beforeEach(() {
+        presenter.animationDidFinish.add(null);
+      });
+
+      context("when authentication exists", () {
+        beforeEach(() {
+          presenter.authenticationDidValidate.add(true);
+        });
+
+        it("triggers router to replace to Home screen", () {
+          verify(router.replaceToHomeScreen(context: anyNamed("context")))
+              .called(1);
+        });
+      });
+
+      context("when authentication doesn't exists", () {
+        beforeEach(() {
+          presenter.authenticationDidValidate.add(false);
+        });
+
+        it("triggers router to replace to Login screen", () {
+          verify(router.replaceToLoginScreen(context: anyNamed("context")))
+              .called(1);
+        });
+      });
+    });
+  });
+}
