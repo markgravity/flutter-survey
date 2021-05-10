51d91828f9e4d628a20acec5eeb06a181784f33d
MarkG <markgravity.in@gmail.com>
2021-05-10 11:14:41 +0700

    chore: add auth service (#17)
    # Conflicts:
    #	lib/services/locator/locator_service_register.dart
    
    # Conflicts:
    #	lib/services/locator/locator_service_register.dart

--- lib/services/locator/locator_service_register.dart
+++ lib/services/locator/locator_service_register.dart
@@ -4,6 +4,7 @@ class LocatorServiceRegister {
   LocatorServiceRegister() {
     locator.registerFactory<HttpService>(() => HttpServiceImpl());
     locator.registerFactory<ApiService>(() => ApiServiceImpl());
+    locator.registerFactory<AuthService>(() => AuthServiceImpl());
     locator.registerFactory<UserApiService>(() => UserApiServiceImpl());
   }
 }
