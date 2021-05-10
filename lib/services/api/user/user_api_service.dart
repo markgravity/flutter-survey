f0de61cbcac144464915a6684b46802bb8e025be
MarkG <markgravity.in@gmail.com>
2021-05-10 11:05:44 +0700

    chore: add user service (#16)
    - move & rename user service

--- lib/services/api/user/user_api_service.dart
+++ lib/services/api/user/user_api_service.dart
@@ -3,20 +3,20 @@ import 'package:survey/services/api/api_service.dart';
 import 'package:survey/services/http/http_service.dart';
 import 'package:survey/services/locator/locator_service.dart';
 
-abstract class UserService {
+abstract class UserApiService {
   static const getProfileEndpoint = "/me";
 
   Future<UserInfo> getProfile();
 }
 
-class UserServiceImpl implements UserService {
+class UserApiServiceImpl implements UserApiService {
   final ApiService _apiService = locator.get();
 
   @override
   Future<UserInfo> getProfile() {
     return _apiService.call(
       method: HttpMethod.get,
-      endPoint: UserService.getProfileEndpoint,
+      endPoint: UserApiService.getProfileEndpoint,
     );
   }
 }
--- lib/services/locator/locator_service_register.dart
+++ lib/services/locator/locator_service_register.dart
@@ -4,6 +4,6 @@ class LocatorServiceRegister {
   LocatorServiceRegister() {
     locator.registerFactory<HttpService>(() => HttpServiceImpl());
     locator.registerFactory<ApiService>(() => ApiServiceImpl());
-    locator.registerFactory<UserService>(() => UserServiceImpl());
+    locator.registerFactory<UserApiService>(() => UserApiServiceImpl());
   }
 }
