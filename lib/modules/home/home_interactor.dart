3cb2b5851b53d538b3f995bb7910373ec2cd6250
MarkG <markgravity.in@gmail.com>
2021-06-09 13:30:57 +0700

    feat: integrate home screen (#26)
    # Conflicts:
    #	lib/modules/home/components/slide_item.dart
    
    # Conflicts:
    #	lib/modules/home/components/top_bar.dart
    #	lib/modules/home/home_view.dart

--- lib/modules/home/home_interactor.dart
+++ lib/modules/home/home_interactor.dart
@@ -1,7 +1,44 @@
 part of 'home_module.dart';
 
-abstract class HomeInteractorDelegate {}
+abstract class HomeInteractorDelegate {
+  BehaviorSubject<bool> get isSurveysCachedDidGet;
 
-abstract class HomeInteractor extends Interactor<HomeInteractorDelegate> {}
+  BehaviorSubject<List<SurveyInfo>> get surveysDidFetch;
 
-class HomeInteractorImpl extends HomeInteractor {}
+  BehaviorSubject<Exception> get surveysDidFailToFetch;
+
+  BehaviorSubject<UserInfo> get authenticatedUserDidGet;
+}
+
+abstract class HomeInteractor extends Interactor<HomeInteractorDelegate> {
+  void getIsSurveysCached();
+
+  void fetchSurveys({bool force = false});
+
+  void getAuthenticatedUser();
+}
+
+class HomeInteractorImpl extends HomeInteractor {
+  final SurveyRepository _surveyRepository = locator.get();
+  final AuthRepository _authRepository = locator.get();
+
+  @override
+  void getIsSurveysCached() {
+    _surveyRepository.isSurveysCached
+        .then((value) => delegate?.isSurveysCachedDidGet.add(value));
+  }
+
+  @override
+  void fetchSurveys({bool force = false}) {
+    _surveyRepository
+        .fetchSurveys(force: force)
+        .then((value) => delegate?.surveysDidFetch.add(value))
+        .onError<Exception>(
+            (exception, _) => delegate?.surveysDidFailToFetch.add(exception));
+  }
+
+  @override
+  void getAuthenticatedUser() {
+    delegate?.authenticatedUserDidGet.add(_authRepository.user!);
+  }
+}
